70c90f42aceb457428241e9b62d3fad6a49101eb
geodevcode <olatunde336@gmail.com>
2022-11-05 17:58:03 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+
+
+function App() {
+  // const [count, setCount] = useState(0);
+  const [userValue, setUserValue] = useState(0);
+
+  function increment(){
+    setUserValue(userValue +10);
+  }
+  function decrement(){
+    setUserValue(userValue -10);
+  }
+  function reset(){
+    setUserValue(0);
+  }
+  
+  function handleChange(event){
+    setUserValue(event.target.value);
+  }
+  
+  return(
+    <div className="container">
+      <h1 className="counting-heading">Counting App</h1>
+      <h1>{userValue}</h1>
+      <button className="increase" onClick={increment}>+</button>
+      <button className="decrease" onClick={decrement}>-</button>
+      <button className="reset" onClick={reset}>Reset</button>
+      <div className="setvalue-cont">
+        <input onChange={handleChange} type ="number" placeholder="Type Your Counting Number"/>
+        <button  className="values">Set Value </button>
+      </div>
+    </div>
+  );
+  
+}
+
+export default App;
